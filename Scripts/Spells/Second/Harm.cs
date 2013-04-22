f5d79477ac5364476df59180af7220f5752cdd13
daveloper <zdaryl@gmail.com>
2013-04-22 05:43:12 +0200

    Revert "Tweaked ReflectSpell cast time"
    This reverts commit 698db74f4b4cb7d0807bd1b3fea52b08c9f895a3.

--- Scripts/Spells/Second/Harm.cs
+++ Scripts/Spells/Second/Harm.cs
@@ -25,7 +25,7 @@ namespace Server.Spells.Second
         //Loki edit: New PvP changes
         public override TimeSpan GetCastDelay()
         {
-            return TimeSpan.FromSeconds(3.0);
+            return TimeSpan.FromSeconds(2.1);
         }
 
         public override void OnPlayerCast()
