7ed4657321a490d1a7a697a42fb1ced57ac28005
rberiot <remy.beriot[at]gmail.com>
2013-04-23 14:08:45 +0200

    renamed server

--- Scripts/Misc/ServerList.cs
+++ Scripts/Misc/ServerList.cs
@@ -39,7 +39,7 @@ namespace Server.Misc
 		 */
 
         	public static readonly string Address = null;
-		public static readonly string ServerName = "Imagine Nation";
+		public static readonly string ServerName = "XUO Fight Club";
 
 		public static readonly bool AutoDetect = true;
 
